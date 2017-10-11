c999a319fb7ec9e3913cf662c49953e1df5bb33c
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:37 +0200

    object to class

--- src/test/scala/nl/biopet/tools/findoverlapmatch/FindOverlapMatchTest.scala
+++ src/test/scala/nl/biopet/tools/findoverlapmatch/FindOverlapMatchTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.findoverlapmatch
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object FindOverlapMatchTest extends BiopetTest {
+class FindOverlapMatchTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
